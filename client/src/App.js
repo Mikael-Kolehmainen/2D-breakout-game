a346bc8354f27c54a30bae018ea7c096b8b965ab
Mikael Kolehmainen <mikaelkolehmainen05@gmail.com>
2023-04-22 11:41:15 +0300

    Created a Game component

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,5 @@
 import React from "react";
-import Canvas from "./components/Canvas";
-import Ball from "./components/Ball";
-import Bricks from "./components/Bricks";
-import { CANVAS } from "./const";
-import Paddle from "./components/Paddle";
-import UIText from "./components/UIText";
+import Game from "./components/Game";
 import Leaderboard from "./components/Leaderboard";
 import "./styles/css/main.css";
 
@@ -14,111 +9,19 @@ import "./styles/css/main.css";
 // Leaderboard
   // Ability to save score with initials
   // Leaderboard is shown below game
+// Known bugs
+  // The ball and paddle are faster on faster devices
 // Clean the code
 // Add jsdoc to code
 // Write readme in Github
 
 function App() {
-  const bricksLayout = new Bricks(3, 9, 37.5, 20, 10, 30, 30);
-  const ball = new Ball(CANVAS.width / 2, CANVAS.height - 30, 2, 2, 10);
-  const paddle = new Paddle((CANVAS.width - 75) / 2, 10, 75);
-
-  let score = 0;
-  let startTime = new Date();
-
-  const draw = (ctx, frameCount) => {
-    ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
-    drawBricks(ctx);
-    drawBall(ctx);
-    drawPaddle(ctx);
-    drawTimer(ctx);
-
-    collisionDetection();
-
-    defineBorders();
-    defineLosingCondition();
-
-    updateBallPosition();
-  };
-
-  const drawBricks = (ctx) => {
-    bricksLayout.ctx = ctx;
-    bricksLayout.draw();
-  };
-
-  const drawBall = (ctx) => {
-    ball.ctx = ctx;
-    ball.draw();
-  };
-
-  const drawPaddle = (ctx) => {
-    paddle.ctx = ctx;
-    paddle.draw();
-    paddle.controller();
-  };
-
-  const drawTimer = (ctx) => {
-    const endTime = new Date();
-    const elapsedTime = endTime - startTime;
-    const elapsedTimeInSeconds = Math.floor(elapsedTime / 1000);
-
-    const timerText = new UIText(ctx, "Time: " + elapsedTimeInSeconds, "16px Arial", "#0095DD", 8, 20);
-    timerText.draw();
-  };
-
-  const collisionDetection = () => {
-    for(let row = 0; row < bricksLayout.rowCount; row++) {
-      for(let column = 0; column < bricksLayout.columnCount; column++) {
-        let brick = bricksLayout.bricks[row][column];
-        if(brick.status === 1) {
-          if(ball.x > brick.x && ball.x < brick.x + bricksLayout.width
-            && ball.y > brick.y && ball.y < brick.y + bricksLayout.height) {
-            ball.dy = -ball.dy;
-            brick.status = 0;
-            score++;
-            if(score === bricksLayout.rowCount * bricksLayout.columnCount) {
-              const endTime = new Date();
-              const elapsedTime = endTime - startTime;
-              console.log(elapsedTime);
-              alert("YOU WIN, CONGRATS!");
-              document.location.reload();
-            }
-          }
-        }
-      }
-    }
-  };
-
-  const defineBorders = () => {
-    ball.defineBorders();
-  };
-
-  const defineLosingCondition = () => {
-    if (ball.y + ball.dy > CANVAS.height - ball.radius) {
-      if (ball.x > paddle.x && ball.x < paddle.x + paddle.width) {
-        ball.dy = -ball.dy;
-      } else {
-        alert("GAME OVER");
-        document.location.reload();
-
-        ball.x = CANVAS.width / 2;
-        ball.y = CANVAS.height - 30;
-        ball.dx = 3;
-        ball.dy = -3;
-        paddle.x = (CANVAS.width - paddle.width) / 2;
-        startTime = new Date();
-      }
-    }
-  };
-
-  const updateBallPosition = () => {
-    ball.updatePosition();
-  };
+  console.log("called");
 
   return (
     <div>
       <h1>Breakout Game</h1>
-      <Canvas draw={draw} />
+      <Game />
       <Leaderboard />
     </div>
   );
---
+++ client/src/components/Game.js
@@ -0,0 +1,114 @@
+import React, { useState } from "react";
+import Canvas from "./Canvas";
+import Bricks from "./Bricks";
+import Ball from "./Ball";
+import Paddle from "./Paddle";
+import UIText from "./UIText";
+import { CANVAS } from "./../const";
+
+const Game = () => {
+  const bricksLayout = new Bricks(3, 9, 37.5, 20, 10, 30, 30);
+  const ball = new Ball(CANVAS.width / 2, CANVAS.height - 30, 2, 2, 10);
+  const paddle = new Paddle((CANVAS.width - 75) / 2, 10, 75);
+
+  let score = 0;
+  let startTime = new Date();
+  const [gameState, setGameState] = useState("");
+  let isGameRunning = true;
+
+  const draw = (ctx, frameCount) => {
+    if (isGameRunning) {
+      ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+      drawBricks(ctx);
+      drawBall(ctx);
+      drawPaddle(ctx);
+      drawTimer(ctx);
+
+      collisionDetection();
+
+      defineBorders();
+      defineLosingCondition();
+
+      updateBallPosition();
+    }
+  };
+
+  const drawBricks = (ctx) => {
+    bricksLayout.ctx = ctx;
+    bricksLayout.draw();
+  };
+
+  const drawBall = (ctx) => {
+    ball.ctx = ctx;
+    ball.draw();
+  };
+
+  const drawPaddle = (ctx) => {
+    paddle.ctx = ctx;
+    paddle.draw();
+    paddle.controller();
+  };
+
+  const drawTimer = (ctx) => {
+    const endTime = new Date();
+    const elapsedTime = endTime - startTime;
+    const elapsedTimeInSeconds = Math.floor(elapsedTime / 1000);
+
+    const timerText = new UIText(ctx, "Time: " + elapsedTimeInSeconds, "16px Arial", "#0095DD", 8, 20);
+    timerText.draw();
+  };
+
+  const collisionDetection = () => {
+    for(let row = 0; row < bricksLayout.rowCount; row++) {
+      for(let column = 0; column < bricksLayout.columnCount; column++) {
+        let brick = bricksLayout.bricks[row][column];
+        if(brick.status === 1) {
+          if(ball.x > brick.x && ball.x < brick.x + bricksLayout.width
+            && ball.y > brick.y && ball.y < brick.y + bricksLayout.height) {
+            ball.dy = -ball.dy;
+            brick.status = 0;
+            score++;
+            if(score === bricksLayout.rowCount * bricksLayout.columnCount) {
+              const endTime = new Date();
+              const elapsedTime = endTime - startTime;
+              console.log(elapsedTime);
+              setGameState("YOU WIN, CONGRATS!");
+              isGameRunning = false;
+            }
+          }
+        }
+      }
+    }
+  };
+
+  const defineBorders = () => {
+    ball.defineBorders();
+  };
+
+  const defineLosingCondition = () => {
+    if (ball.y + ball.dy > CANVAS.height - ball.radius) {
+      if (ball.x > paddle.x && ball.x < paddle.x + paddle.width) {
+        ball.dy = -ball.dy;
+      } else {
+        setGameState("GAME OVER");
+        console.log("test");
+        isGameRunning = false;
+
+        ball.x = CANVAS.width / 2;
+        ball.y = CANVAS.height - 30;
+        ball.dx = 3;
+        ball.dy = -3;
+        paddle.x = (CANVAS.width - paddle.width) / 2;
+        startTime = new Date();
+      }
+    }
+  };
+
+  const updateBallPosition = () => {
+    ball.updatePosition();
+  };
+
+  return <Canvas draw={draw} />;
+};
+
+export default Game;
